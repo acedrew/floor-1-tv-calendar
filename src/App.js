81d05335d2377375c131c6bfc5256f98a55f3c24
Andrew Rodgers <acedrew@acedrew.com>
2016-10-01 13:43:52 -0400

    First draft
    Floor 1 Signage

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import GetEvents from './getEvents';
+import TitleAndTime from './titleAndTime';
+
+function getBOD() {
+  let start = new Date();
+  start.setHours(0);
+  start.setMinutes(0);
+  start.setSeconds(0);
+  start.setMilliseconds(0);
+  return start.toISOString();
+}
+
+function getEOD() {
+  let end = new Date();
+  end.setHours(23);
+  end.setMinutes(59);
+  end.setSeconds(59);
+  end.setMilliseconds(999);
+  return end.toISOString();
+}
+
+
+function makeGoogleCalendarURL(calID) {
+  return `https://www.googleapis.com/calendar/v3/calendars/${calID}/events?singleEvents=true&orderBy=startTime&timeMin=${getBOD()}&timeMax=${getEOD()}&key=AIzaSyDFzoxhmCRgYGWBzxhjClogyoh0ibNlhPs`
+}
+
+const floors = [
+  {
+    name: "Rooftop",
+    spaces: [
+      {
+        url: "theedney.com_4172gjp6l679ntf1m5t94c3nls@group.calendar.google.com",
+        name: "Rooftop Deck"
+      }
+    ]
+  },
+  {
+    name: "Floor 5",
+    spaces: [
+      {
+        url:  "theedney.com_lsgotg320507m2s8tnchqsd1v0@group.calendar.google.com",
+        name: "Community Space"
+      },
+      {
+        url:  "theedney.com_acrpuqqea0sus4irgf8m52466c@group.calendar.google.com",
+        name: "Accelerator Space"
+      },
+      {
+        url:  "theedney.com_ebmlbbh0qcu7u6dgdc98ti58k4@group.calendar.google.com",
+        name: "Large Conference Room"
+      },
+      {
+        url: "theedney.com_3s22k2kouob2co8kdt6gqkg14g@group.calendar.google.com",
+        name: "Small Conference Room"
+      }
+    ]
+  },
+  {
+    name: "Floor 3",
+    spaces: [
+      {
+        url: "theedney.com_58b6jopvgat4f974mitp8fcn9k@group.calendar.google.com",
+        name: "Floor 3 East"
+      },
+      {
+        url: "theedney.com_am2ggtaf08u44vgrb2hqbtimmc@group.calendar.google.com",
+        name: "Floor 3 West"
+      }
+    ]
+  },
+  {
+    name: "Floor 1",
+    spaces: [
+      {
+        url: "theedney.com_humeelae3o2gltq8gi7sdpu0js@group.calendar.google.com",
+        name: "Floor 1 Lobby"
+      }
+    ]
+  }
+].map(function(floor) {
+  floor.spaces = floor.spaces.map(function(space) {
+    space.url = makeGoogleCalendarURL(space.url);
+    return space;
+  });
+  if (floor.name == "Floor 1") {
+    floor.ligature = "➤"
+  } else {
+    floor.ligature = "⮝"
+  }
+  return floor;
+});
+console.log(floors);
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TitleAndTime/>
+        <div className="side-padding">
+          {floors.map(floor =>
+            <div key={floor.name} className="grid set-height">
+            <div className="la">
+              <h2 className="event-space">
+                <div className="left-arrow">
+                {floor.ligature}
+                </div>
+                {floor.name}
+              </h2>
+              {floor.spaces.map(space =>
+              <GetEvents key={space.url} SpacesCalURL={space.url}/>
+            )}
+            </div>
+          </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/getEvents.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class GetEvents extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: []
+    };
+  }
+
+
+  getEvents(url) {
+    axios.get(url)
+      .then(res => {
+        const events = res.data.items.map(function(event) {
+          let AMPM, endHours, startHours, endAMPM, startAMPM;
+          let startTime = new Date(event.start.dateTime);
+          let endTime = new Date(event.end.dateTime);
+          let time = new Date();
+
+          function pad(n) {
+            return (n < 10) ? ("0" + n) : n;
+          }
+
+          function twentyfourtotwelve(hours) {
+
+            if ( hours > 11 ){
+              AMPM = "PM";
+            } else {
+              AMPM = "AM";
+            }
+            hours = hours === 12 ? 12 : hours % 12
+            return [hours, AMPM];
+          }
+          [startHours, startAMPM] = twentyfourtotwelve(startTime.getHours());
+          [endHours, endAMPM] = twentyfourtotwelve(endTime.getHours());
+
+          if(startAMPM === endAMPM) {
+            event.start.time = `${startHours}:${pad(startTime.getMinutes())}`;
+            event.end.time = `${endHours}:${pad(endTime.getMinutes())} ${endAMPM}`;
+          } else {
+            event.start.time = `${startHours}:${pad(startTime.getMinutes())} ${startAMPM}`;
+            event.end.time = `${endHours}:${pad(endTime.getMinutes())} ${endAMPM}`;
+          }
+
+          if (endTime.getTime() < time.getTime()) {
+            event.class = "past";
+          } else {
+            event.class = "current";
+          }
+          return event;
+        });
+        this.setState({ events });
+      });
+    }
+  queryEvents() {
+    console.log(this.props.SpacesCalUrl);
+    this.getEvents(this.props.SpacesCalURL);
+  }
+
+  componentDidMount() {
+    this.queryEvents();
+    window.setInterval(function () {
+      this.queryEvents();
+    }.bind(this), 5 * 60 * 1000);
+  }
+
+  render() {
+      return (
+        <div>
+          {this.state.events.map(event =>
+            <div key={event.id} className={event.class}>
+              <p className="event-title">{event.summary}</p>
+              <p className="event-time">{event.start.time} - {event.end.time}</p>
+            </div>
+          )}
+          </div>
+      )
+    }
+  }
